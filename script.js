cbd819d3b2afde88e989f602287b8eae8d88f43f
bishald0013 <bishal986473@gmail.com>
2023-01-25 20:51:40 +0530

    animation frame increased

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
-myCanvas.width=400;
-myCanvas.height=300;
-const margin=30;
-const n = 20;
+myCanvas.width = 400
+myCanvas.height = 300
+const margin = 30
+const n = 20
 const array = []
-
-for(let i=0; i<n; i++){
-    array[i]=Math.random();
-}
+let moves = []
 
 const cols = []
-const spacing = (myCanvas.width-margin*2)/n;
-const ctx = myCanvas.getContext('2d');
+const spacing = (myCanvas.width - margin * 2) / n
+const ctx = myCanvas.getContext('2d')
+const maxColumnHeight = 200
 
-const maxColumnHeight=200
+function init() {
+  for (let i = 0; i < n; i++) {
+    array[i] = Math.random()
+  }
+  moves = []
 
-//display all the value of canvas in deff location
-for(let i=0; i<array.length; i++){
-    const x = i*spacing+spacing/2+margin;
-    const y = myCanvas.height-margin-i*3;
-    const width = spacing-4;
-    const height=maxColumnHeight*array[i];
-    cols[i] = new Column(x,y,width,height);
+  //display all the value of canvas in deff location
+  //creating columns
+  for (let i = 0; i < array.length; i++) {
+    const x = i * spacing + spacing / 2 + margin
+    const y = myCanvas.height - margin - i * 3
+    const width = spacing - 4
+    const height = maxColumnHeight * array[i]
+    cols[i] = new Column(x, y, width, height)
     // cols[i].draw(ctx);
+  }
 }
 
-let moves=bubbleSort(array)
-console.log(moves)
+function play() {
+  moves = bubbleSort(array)
+}
+
+
 
 animate()
 
 //bubble-sort algorithm
-function bubbleSort(){
-    const moves = []
-    do {
-        var swapped=false;
-        for(let i=1;i<array.length;i++){
-            if(array[i-1]>array[i]){
-                swapped=true;
-                [array[i-1],array[i]]=[array[i],array[i-1]];
-                moves.push(
-                    {indices:[i-1,i],swap:true}
-                );
-            }else{
-                moves.push(
-                    {indices:[i-1,i],swap:false}
-                );
-            }
-        }
-    } while (swapped);
+function bubbleSort() {
+  const moves = []
+  do {
+    var swapped = false
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1] > array[i]) {
+        swapped = true
+        ;[array[i - 1], array[i]] = [array[i], array[i - 1]]
+        moves.push({ indices: [i - 1, i], swap: true })
+      } else {
+        moves.push({ indices: [i - 1, i], swap: false })
+      }
+    }
+  } while (swapped)
 
-    return moves;
+  return moves
 }
 
 //animate frames
-function animate(){
-    ctx.clearRect(0,0,myCanvas.width,myCanvas.height)
-    let changed = false
-    for(let i=0; i<cols.length;i++){
-        changed=cols[i].draw(ctx)||changed
+function animate() {
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height)
+  let changed = false
+  for (let i = 0; i < cols.length; i++) {
+    changed = cols[i].draw(ctx) || changed
+  }
+
+  if (!changed && moves.length > 0) {
+    const move = moves.shift()
+    const [i, j] = move.indices
+    if (move.swap) {
+      cols[i].moveTo(cols[j])
+      cols[j].moveTo(cols[i], -1)
+      ;[cols[i], cols[j]] = [cols[j], cols[i]]
+    } else {
     }
+  }
 
-    if(!changed && moves.length>0){
-        const move=moves.shift()
-        const [i,j]=move.indices;
-        if(move.swap){
-            cols[i].moveTo(cols[j]);
-            cols[j].moveTo(cols[i],-1);
-            [cols[i],cols[j]]=[cols[j],cols[i]];
-        }else{
-
-        }
-    }
-
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate)
 }
-
